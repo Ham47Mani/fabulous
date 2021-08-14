14d6b57e2bc6f6e1baa5c2f9a36d4c8d1df29b62
Ham47mani <ham47mani@gmail.com>
2021-08-14 22:54:23 +0100

    Upload project files

---
+++ js/plugins.js
@@ -0,0 +1,28 @@
+/*
+* Template Name      : Fabulous
+ * Template Version   : V 1.0.0
+ * Template Url       : .....................
+ * Language           : HTML5 | CSS3 |JavaScript
+ * Framework          : jQuery [JavaScript]
+ * Preprocesseur      : SaSS [CSS]
+ * Contact Me : Skaype   :- Boudraa.Hammani
+ *				Facebook :- Ham mani
+ *				Email    :- Ham47mani@gmail.com
+ */
+/*global $*/
+$(function () {
+    "use strict";
+
+    /*                       Start Skitter Slider
+    ========================================================================== */
+    $(".box_skitter_large").skitter({
+        theme: "clean",
+        dots: true,
+        preview: true,
+        numbers_align: "center",
+        progressbar: true,
+        progressbar_css: { backgroundColor: 'var(--main-Color)' }
+    });
+    /*                       End Skitter Slider
+    ========================================================================== */
+});
